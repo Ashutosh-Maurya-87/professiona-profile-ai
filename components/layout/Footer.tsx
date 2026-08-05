94f4bccfe31e9d9e262c3b2c897ac8aab3dfb5fd
Ashutosh Maurya <ashumaurya486@gmail.com>
2026-08-05 11:34:32 +0530

    update footer componnet

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return <footer>
+        <div className="wrap">
+            <p>© 2026 Ashutosh Maurya. Built with React-grade attention to detail.</p>
+            <p>Ayodhya, U.P., India</p>
+        </div>
+    </footer>
+}
+export default Footer
