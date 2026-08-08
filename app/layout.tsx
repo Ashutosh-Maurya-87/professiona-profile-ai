136b6fb09b04aa5c9fc9670fa1c1ed5d08865641
Ashutosh Maurya <ashumaurya486@gmail.com>
2026-08-08 15:01:45 +0530

    Added analaytics for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 import ThemeProvider from "@/providers/ThemeProvider";
@@ -9,6 +10,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6GQJDJR7WJ"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6GQJDJR7WJ');`}
+        </script>
+      </head>
       <body>
         <ThemeProvider>
           {children}
